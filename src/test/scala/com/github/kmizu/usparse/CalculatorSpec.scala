263fc1ed08f07950c646595026755b575396d21e
Kota Mizushima <mizukota@gmail.com>
2019-01-18 15:03:51 +0900

    Fix example

--- src/test/scala/com/github/kmizu/usparse/CalculatorSpec.scala
+++ src/test/scala/com/github/kmizu/usparse/CalculatorSpec.scala
@@ -5,17 +5,18 @@ import org.scalatest.{DiagrammedAssertions, FunSpec}
 class CalculatorSpec extends FunSpec with DiagrammedAssertions {
   object Calculator {
     def E: P[Int] = rule(A)
-    def A: Parser[Int] = rule(chainl(M) {
-      $("+").map { op => (lhs: Int, rhs: Int) => lhs + rhs } |
-      $("-").map { op => (lhs: Int, rhs: Int) => lhs - rhs }
-    })
-    def M: Parser[Int] = rule(chainl(P) {
-      $("*").map { op => (lhs: Int, rhs: Int) => lhs * rhs } |
-      $("/").map { op => (lhs: Int, rhs: Int) => lhs / rhs }
-    })
+    def A: Parser[Int] = rule {
+      M ~ ($("+") ~ M | $("-") ~ M).* ^^ {case x ~ ys =>
+        ys.foldLeft(x){ case (l, op ~ r) => if(op == "+") l + r else l -r }
+      }
+    }
+    def M: Parser[Int] = rule {
+      P ~ ($("*") ~ P | $("/") ~ P).* ^^ { case x ~ ys =>
+        ys.foldLeft(x) { case (l, op ~ r) => if (op == "*") l * r else l / r }
+      }
+    }
     def P: P[Int] = rule{
-      (for {
-        _ <- $("("); e <- E; _ <- $(")")} yield e) | number
+      (for { _ <- $("("); e <- E; _ <- $(")")} yield e) | number
     }
     def number: P[Int] = rule {
       ('0' to '9').map{c => $(c.toString) ^^ (_.toInt)}.reduce((p1, p2) => p1 | p2)
@@ -36,7 +37,7 @@ class CalculatorSpec extends FunSpec with DiagrammedAssertions {
     }
     it("cannot parse incorrect expressions, which ends with unexpected EOF") {
       input = "1+ "
-      assert(parser(input) == Success(1, " "))
+      assert(parser(input) == Success(1, "+ "))
     }
 
     it("cannot parse incorrect expressions, which contains spaces") {
